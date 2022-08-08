3884c5fa0dee57e71e1fd975b799397daeeeb286
rohitraj0007 <44340474+rohitraj0007@users.noreply.github.com>
2022-08-08 17:13:25 +0530

    summary added

---
+++ jdbc-demo/src/test/java/com/senseofcode/demo/jdbcdemo/JdbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senseofcode.demo.jdbcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
