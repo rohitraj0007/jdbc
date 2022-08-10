ea93109f786ff7b64f350d68683a7d3db6feade7
rohitraj0007 <44340474+rohitraj0007@users.noreply.github.com>
2022-08-10 17:13:17 +0530

    jdbc code added

--- jdbc-demo/src/main/java/com/senseofcode/demo/jdbcdemo/JdbcDemoApplication.java
+++ jdbc-demo/src/main/java/com/senseofcode/demo/jdbcdemo/JdbcDemoApplication.java
@@ -1,13 +1,39 @@
 package com.senseofcode.demo.jdbcdemo;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JdbcDemoApplication {
+import com.senseofcode.demo.jdbcdemo.entity.Person;
+import com.senseofcode.demo.jdbcdemo.repository.PersonJdbcDao;
+import com.senseofcode.demo.jdbcdemo.repository.PersonPureJdbcDao;
 
+@SpringBootApplication
+public class JdbcDemoApplication implements CommandLineRunner {
+
+	
+	@Autowired
+	PersonJdbcDao personJdbcDao;
+	
+	@Autowired
+	PersonPureJdbcDao personPureJdbcDao ;
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(personJdbcDao.findAll());
+		System.out.println(personJdbcDao.findById(10002));
+		System.out.println(personJdbcDao.deleteById(10002));
+		System.out.println(personJdbcDao.insertIntoPerson(new Person(10004,"Siyaldah","Amit",new Date())));
+		System.out.println(personJdbcDao.updatePerson(new Person(10004,"Siyalda","Amit Chowdhary",new Date())));
+		System.out.println(personJdbcDao.findByIdUsingCustomeRowMapper(10004));
+		System.out.println(personPureJdbcDao.findAll());
+		
 	}
 
 }
